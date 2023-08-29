6b6dfad09a80ea83aff84579c1c67d07383908ac
MBvisti <mbv@mortenvistisen.com>
2023-08-29 12:51:46 +0200

    remove api token

--- cmd/app/main.go
+++ cmd/app/main.go
@@ -1,7 +1,6 @@
 package main
 
 import (
-	"log"
 	"os"
 
 	"github.com/MBvisti/grafto/controllers"
@@ -23,17 +22,11 @@ func main() {
 	router.Use(middleware.Recover())
 	conn := database.SetupDatabaseConnection(os.Getenv("DATABASE_URL"))
 	db := database.New(conn)
-	controllers := controllers.NewController(*db)
 
-	postmark := mail.NewPostmark("332f9c6b-aac6-426d-b070-8eadaccce28c")
+	postmark := mail.NewPostmark(os.Getenv("POSTMARK_API_TOKEN"))
+
 	mailClient := mail.NewMail(&postmark)
-
-	err := mailClient.Send("confirm_password", mail.ConfirmPassword{Token: "yoyoyoyoyoy"})
-	if err != nil {
-		panic(err)
-	}
-
-	log.Print(err)
+	controllers := controllers.NewController(*db, mailClient)
 
 	server := routes.NewServer(router, v, controllers)
 
