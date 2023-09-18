aa6cee5a3864b95429388a65e4752a1c331f31ff
MBvisti <mbv@mortenvistisen.com>
2023-09-18 17:10:16 +0200

    cleaning

--- routes/middleware/middleware.go
+++ routes/middleware/middleware.go
@@ -1,20 +1,12 @@
 package middleware
 
 import (
-	"github.com/gorilla/csrf"
 	"github.com/labstack/echo/v4"
 )
 
 // AuthOnly is only a placeholder for now
 func AuthOnly(next echo.HandlerFunc) echo.HandlerFunc {
 	return func(c echo.Context) error {
-		return next(c)
-	}
-}
-func CsrfMiddlewareFunc(next echo.HandlerFunc) echo.HandlerFunc {
-	return func(c echo.Context) error {
-		csrfToken := csrf.Token(c.Request())
-		c.Response().Header().Set(echo.HeaderXCSRFToken, csrfToken)
 		return next(c)
 	}
 }
