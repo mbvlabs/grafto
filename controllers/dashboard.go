7240f95c27c493410dcced5508db49579f0691cb
MBvisti <mbv@mortenvistisen.com>
2023-11-16 18:06:29 +0100

    wip

--- controllers/dashboard.go
+++ controllers/dashboard.go
@@ -1,13 +1,10 @@
 package controllers
 
 import (
+	"github.com/MBvisti/grafto/views"
 	"github.com/labstack/echo/v4"
 )
 
 func (c *Controller) DashboardIndex(ctx echo.Context) error {
-	// return ctx.Render(http.StatusOK, "dashboard/index", views.RenderOpts{
-	// 	Layout: views.DashboardLayout,
-	// 	Data:   nil,
-	// })
-	return nil
+	return views.Dashboard(ctx)
 }
