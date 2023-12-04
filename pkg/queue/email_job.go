94124fe4238a65d2aa6a9e5d58b65fc136b6d75e
MBvisti <mbv@mortenvistisen.com>
2023-12-04 21:01:12 +0100

    wip

--- pkg/queue/email_job.go
+++ pkg/queue/email_job.go
@@ -3,6 +3,7 @@ package queue
 import (
 	"context"
 	"encoding/json"
+	"time"
 )
 
 const emailExecutorName string = "email_executor"
@@ -23,8 +24,6 @@ func NewEmailExecutor(client emailSender) *emailExecutor {
 	}
 }
 
-var _ Executor = (*emailExecutor)(nil)
-
 // Name implements Executor.
 func (e *emailExecutor) Name() string {
 	return e.name
@@ -32,7 +31,7 @@ func (e *emailExecutor) Name() string {
 
 // Process implements Executor.
 func (e *emailExecutor) process(ctx context.Context, msg []byte) error {
-	var instructions EmailJob
+	var instructions emailJob
 	if err := json.Unmarshal(msg, &instructions); err != nil {
 		return err
 	}
@@ -46,24 +45,52 @@ func (e *emailExecutor) process(ctx context.Context, msg []byte) error {
 	return nil
 }
 
-type EmailJob struct {
+var _ Executor = (*emailExecutor)(nil)
+
+type emailJob struct {
 	To       string      `json:"to"`
 	From     string      `json:"from"`
 	Subject  string      `json:"subject"`
 	TmplName string      `json:"tmpl_name"`
 	Payload  interface{} `json:"payload"`
+	job      *job
 }
 
-func NewEmailJob(payload EmailJob) (*Job, error) {
-	instructions, err := json.Marshal(payload)
+var _ jobCreator = (*emailJob)(nil)
+
+func NewEmailJob(to, from, subject, tmplName string, payload any) (*emailJob, error) {
+	ej := emailJob{
+		To:       to,
+		From:     from,
+		Subject:  subject,
+		TmplName: tmplName,
+		Payload:  payload,
+	}
+
+	instructions, err := json.Marshal(ej)
 	if err != nil {
 		return nil, err
 	}
 
-	jobInstructions := jobInstructions{
+	job, err := newJob(jobInstructions{
 		instructions: instructions,
 		executor:     emailExecutorName,
+	})
+	if err != nil {
+		return nil, err
 	}
 
-	return newJob(jobInstructions)
+	ej.job = job
+
+	return &ej, nil
+}
+
+func (e *emailJob) Schedule(t time.Time) error {
+	// check if t is in the past
+	e.job.scheduledFor = t
+	return nil
+}
+
+func (e *emailJob) build() *job {
+	return e.job
 }
