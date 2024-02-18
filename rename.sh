424656f567030f04134ebce115b74bada3403e89
Morten Vistisen <mbv@mortenvistisen.com>
2024-02-18 17:01:19 +0100

    chore: add script to rename and self destroy (#80)

---
+++ rename.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+echo "this will replace mbv-labs with your project name in all files"
+
+echo "Enter your project name: "
+
+read project_name
+
+echo "replacing mbv-labs with $project_name"
+
+find . -type f -name "*.go" -exec sed -i'' -e "s/mbv-labs/$project_name/g" {} +
+find . -type f -name "*.templ" -exec sed -i'' -e "s/mbv-labs/$project_name/g" {} +
+
+echo "deleting rename.sh"
+
+rm rename.sh
