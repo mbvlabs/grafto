a40b0a1f5ca103c178d0dfc48ea2e0e14514307d
Morten Vistisen <mbv@mortenvistisen.com>
2024-02-18 17:42:37 +0100

    fix: make renaming work (#81)

--- rename.sh
+++ rename.sh
@@ -1,15 +1,26 @@
 #!/bin/bash
 
-echo "this will replace mbv-labs with your project name in all files"
+echo "this will replace mbv-labs with your gh_profile and project name in all files"
+
+echo "Enter your gh_profile name: "
+
+read gh_profile_name
+
+echo "replacing mbv-labs with $gh_profile_name"
+
+find . -type f -name "*.go" -exec sed -i'' -e "s/mbv-labs/$gh_profile_name/g" {} +
+find . -type f -name "go.mod" -exec sed -i'' -e "s/mbv-labs/$gh_profile_name/g" {} +
+find . -type f -name "*.templ" -exec sed -i'' -e "s/mbv-labs/$gh_profile_name/g" {} +
 
 echo "Enter your project name: "
 
 read project_name
 
-echo "replacing mbv-labs with $project_name"
+echo "replacing grafto with $project_name"
 
-find . -type f -name "*.go" -exec sed -i'' -e "s/mbv-labs/$project_name/g" {} +
-find . -type f -name "*.templ" -exec sed -i'' -e "s/mbv-labs/$project_name/g" {} +
+find . -type f -name "*.go" -exec sed -i'' -e "s/$gh_profile_name/$project_name/g" {} +
+find . -type f -name "go.mod" -exec sed -i'' -e "s/$gh_profile_name/$project_name/g" {} +
+find . -type f -name "*.templ" -exec sed -i'' -e "s/$gh_profile_name/$project_name/g" {} +
 
 echo "deleting rename.sh"
 
