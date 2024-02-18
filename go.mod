1af64a21982393210349b5a514fd50491b8e6d65
MBvisti <mbv@mortenvistisen.com>
2024-02-18 18:01:27 +0100

    fix: remove leftover sleep in health endpoint

--- go.mod
+++ go.mod
@@ -44,7 +44,6 @@ require (
 	github.com/jackc/pgx/v5 v5.5.2
 	github.com/labstack/echo/v4 v4.11.1
 	github.com/pkg/errors v0.9.1
-	github.com/robfig/cron/v3 v3.0.1
 	github.com/samber/slog-echo v1.2.1
 	golang.org/x/exp v0.0.0-20230817173708-d852ddb80c63 // indirect
 	golang.org/x/sys v0.15.0 // indirect
--- routes/api.go
+++ routes/api.go
@@ -1,16 +1,13 @@
 package routes
 
 import (
-	"time"
-
+	"github.com/labstack/echo/v4"
 	"github.com/mbv-labs/grafto/controllers"
 	"github.com/mbv-labs/grafto/server/middleware"
-	"github.com/labstack/echo/v4"
 )
 
 func apiRoutes(router *echo.Group, controllers controllers.Controller, middleware middleware.Middleware) {
 	router.GET("/health", func(c echo.Context) error {
-		time.Sleep(6 * time.Second)
 		return controllers.AppHealth(c)
 	})
 
