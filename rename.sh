da61208c380709643475b1a035eef1b2a3cfe785
MBvisti <mbv@mortenvistisen.com>
2024-02-18 16:58:57 +0100

    adding script to rename

--- rename.sh
+++ rename.sh
@@ -8,5 +8,9 @@ read project_name
 
 echo "replacing mbv-labs with $project_name"
 
-find . -type f -name "*.go" -exec sed -i'' -e "s/MBvisti/$project_name/g" {} +
-find . -type f -name "*.templ" -exec sed -i'' -e 's/mbv-labs/$project_name/g' {} +
+find . -type f -name "*.go" -exec sed -i'' -e "s/mbv-labs/$project_name/g" {} +
+find . -type f -name "*.templ" -exec sed -i'' -e "s/mbv-labs/$project_name/g" {} +
+
+echo "deleting rename.sh"
+
+rm rename.sh
