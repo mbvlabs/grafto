b1e8bd7d43a0e5701789fa47b7097365433d53a0
MBvisti <mbv@mortenvistisen.com>
2024-08-05 17:52:47 +0200

    done

--- views/errors.go
+++ views/errors.go
@@ -1,24 +1,3 @@
 package views
 
-// type Error struct {
-// 	Msg      string
-// 	OldValue string
-// }
-//
-// func (e Error) Exists() bool {
-// 	return e.Msg != "" || e.OldValue != ""
-// }
-
-// type Errors map[string]Error
-
 type Errors map[string]string
-
-// import "github.com/labstack/echo/v4"
-//
-// type InternalServerErrData struct {
-// 	FromLocation string
-// }
-//
-// func InternalServerErr(ctx echo.Context, data InternalServerErrData) error {
-// 	return nil
-// }
