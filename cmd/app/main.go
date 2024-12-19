17f468458f298c491e46aeeef06c358d78158b7c
mbv <github@mbvlabs.com>
2024-12-19 14:18:25 +0100

    feat: update app/main.go to use run for testability

--- cmd/app/main.go
+++ cmd/app/main.go
@@ -2,92 +2,113 @@ package main
 
 import (
 	"context"
+	"fmt"
 	"log/slog"
+	"os"
+	"os/signal"
+	"strings"
 
-	"github.com/gorilla/sessions"
+	"github.com/maypok86/otter"
 	"github.com/mbvlabs/grafto/config"
-	"github.com/mbvlabs/grafto/http"
-	"github.com/mbvlabs/grafto/http/handlers"
-	mw "github.com/mbvlabs/grafto/http/middleware"
-	"github.com/mbvlabs/grafto/models"
-	awsses "github.com/mbvlabs/grafto/pkg/aws_ses"
+	"github.com/mbvlabs/grafto/controllers"
+	emails "github.com/mbvlabs/grafto/pkg/email_client"
 	"github.com/mbvlabs/grafto/pkg/telemetry"
 	"github.com/mbvlabs/grafto/psql"
 	"github.com/mbvlabs/grafto/queue"
+	"github.com/mbvlabs/grafto/queue/workers"
 	"github.com/mbvlabs/grafto/routes"
+	"github.com/mbvlabs/grafto/server"
+	mw "github.com/mbvlabs/grafto/server/middleware"
 	"github.com/mbvlabs/grafto/services"
+	"riverqueue.com/riverui"
 )
 
 var appRelease string
 
-func main() {
+func run(ctx context.Context) error {
 	cfg := config.NewConfig()
 
-	otel := telemetry.NewOtel(cfg)
+	ctx, cancel := signal.NotifyContext(ctx, os.Interrupt)
+	defer cancel()
+
+	otel := telemetry.NewOtel()
 	defer func() {
 		if err := otel.Shutdown(); err != nil {
 			panic(err)
 		}
 	}()
 
-	appTracer := otel.NewTracer("app/tracer")
+	// appTracer := otel.NewTracer("app/tracer")
 
-	client := telemetry.NewTelemetry(cfg, appRelease, "grafto")
+	client := telemetry.NewTelemetry(cfg, appRelease, strings.ToLower(cfg.ProjectName))
 	if client != nil {
 		defer client.Stop()
 	}
 
 	conn, err := psql.CreatePooledConnection(
-		context.Background(),
+		ctx,
 		cfg.GetDatabaseURL(),
 	)
 	if err != nil {
-		panic(err)
+		return err
+	}
+	queueWorkers, err := workers.SetupWorkers(workers.WorkerDependencies{})
+	if err != nil {
+		return err
+	}
+	riverClient := queue.NewClient(conn, queue.WithLogger(slog.Default()), queue.WithWorkers(queueWorkers))
+	psql := psql.NewPostgres(conn, riverClient)
+
+	opts := &riverui.ServerOpts{
+		Client: riverClient,
+		DB:     conn,
+		Logger: slog.New(slog.NewJSONHandler(os.Stdout, &slog.HandlerOptions{
+			Level: slog.SetLogLoggerLevel(slog.LevelError),
+		})),
+		Prefix: "/river",
+	}
+	riverUI, err := riverui.NewServer(opts)
+	if err != nil {
+		return err
+	}
+	// Start the server to initialize background processes for caching and periodic queries:
+	if err := riverUI.Start(ctx); err != nil {
+		return err
 	}
 
-	psql := psql.NewPostgres(conn)
-	riverClient := queue.NewClient(conn, queue.WithLogger(slog.Default()))
+	awsSES := emails.NewSESClient()
+	emailClient := emails.NewEmail(awsSES)
 
-	authSessionStore := sessions.NewCookieStore(
-		[]byte(cfg.SessionKey),
-		[]byte(cfg.SessionEncryptionKey),
-	)
-
-	awsSes := awsses.New()
-
-	authSvc := services.NewAuth(psql, authSessionStore, cfg)
+	authSvc := services.NewAuthNew(psql, emailClient)
 	tokenService := services.NewTokenSvc(psql, cfg.TokenSigningKey)
-	emailService := services.NewEmailSvc(cfg, &awsSes, riverClient)
 
-	userModelSvc := models.NewUserService(psql, authSvc)
+	cacheBuilder, err := otter.NewBuilder[string, string](20)
+	if err != nil {
+		return err
+	}
 
-	flashStore := handlers.NewCookieStore("")
-	baseHandler := handlers.NewDependencies(
-		cfg,
+	pageCacher, err := cacheBuilder.WithVariableTTL().Build()
+	if err != nil {
+		return err
+	}
+
+	appHandlers := controllers.NewApp(psql, pageCacher)
+	dashboardHandlers := controllers.NewDashboard()
+	registrationHandlers := controllers.NewRegistration(
+		authSvc,
 		psql,
-		flashStore,
-		riverClient,
-		appTracer,
-	)
-	appHandlers := handlers.NewApp(baseHandler)
-	dashboardHandlers := handlers.NewDashboard(baseHandler)
-	registrationHandlers := handlers.NewRegistration(
-		authSvc,
-		baseHandler,
-		userModelSvc,
 		*tokenService,
-		emailService,
+		emailClient,
 	)
-	apiHandlers := handlers.NewApi()
-	authenticationHandlers := handlers.NewAuthentication(
+	apiHandlers := controllers.NewApi()
+	authenticationHandlers := controllers.NewAuthentication(
 		authSvc,
-		baseHandler,
-		userModelSvc,
+		psql,
 		*tokenService,
-		emailService,
+		emailClient,
 	)
 
-	serverMW := mw.NewMiddleware(authSvc)
+	middleware := mw.NewMiddleware()
 
 	routes := routes.NewRoutes(
 		appHandlers,
@@ -95,14 +116,21 @@ func main() {
 		authenticationHandlers,
 		registrationHandlers,
 		apiHandlers,
-		baseHandler,
-		serverMW,
-		cfg,
+		middleware,
+		riverUI,
 	)
 
 	router := routes.SetupRoutes()
 
-	server := http.NewServer(router, cfg)
+	server := server.NewServer(ctx, router)
 
-	server.Start()
+	return server.Start(ctx)
+}
+
+func main() {
+	ctx := context.Background()
+	if err := run(ctx); err != nil {
+		fmt.Fprintf(os.Stderr, "%s\n", err)
+		os.Exit(1)
+	}
 }
