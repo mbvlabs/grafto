4e4d5aaee9c1ca932724febce97e3cad3c5fedc5
mbv <github@mbvlabs.com>
2024-12-19 13:57:23 +0100

    chore: use postcss instead of tailwindcli

---
+++ resources/tailwind.config.js
@@ -0,0 +1,17 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+	content: [
+		"../views/**/*.templ",
+		"../views/**/*.go",
+	],
+	blocklist: [],
+	darkMode: 'class',
+	daisyui: {
+		themes: ["dracula"],
+	},
+	plugins: [
+		require('@tailwindcss/forms'),
+		require("@tailwindcss/typography"),
+		require('daisyui')
+	],
+}
