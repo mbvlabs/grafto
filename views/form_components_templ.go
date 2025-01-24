6337d7eb2032eec9a5ec93c10aef26c8a42e1dcd
Morten <github@mbvlabs.com>
2025-01-24 15:02:39 +0100

    remove color

--- views/form_components_templ.go
+++ views/form_components_templ.go
@@ -53,7 +53,7 @@ func InputField(title, fieldType, fieldName, placeholder string, attributes temp
 			templ_7745c5c3_Var1 = templ.NopComponent
 		}
 		ctx = templ.ClearChildren(ctx)
-		templ_7745c5c3_Err = templruntime.WriteString(templ_7745c5c3_Buffer, 1, "<label class=\"form-control bg-blue-800 w-full\"><div class=\"label\"><span class=\"label-text md:text-lg\">")
+		templ_7745c5c3_Err = templruntime.WriteString(templ_7745c5c3_Buffer, 1, "<label class=\"form-control w-full\"><div class=\"label\"><span class=\"label-text md:text-lg\">")
 		if templ_7745c5c3_Err != nil {
 			return templ_7745c5c3_Err
 		}
