3883eee0a8bef71c8e6c05e1ab9f0063fa3de027
Morten <github@mbvlabs.com>
2025-02-02 19:04:55 +0100

    chore: clean up seeds

--- models/seeds/seed.go
+++ models/seeds/seed.go
@@ -2,18 +2,7 @@ package seeds
 
 import (
 	"github.com/mbvlabs/grafto/models/internal/db"
-	"golang.org/x/net/context"
 )
-
-type SeedBuilder[T, V any] interface {
-	WithRandoms(n int) *T
-	WithSpecific(data map[string]any) *T
-	Build() *V
-}
-
-type Seed interface {
-	Generate(ctx context.Context, dbtx db.DBTX) error
-}
 
 type Seeder struct {
 	dbtx db.DBTX
