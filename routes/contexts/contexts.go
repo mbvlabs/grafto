7d7e55791bb49cfc91ea35b5050368758f73063b
Morten Vistisen <mbv@mortenvistisen.com>
2025-03-21 21:30:37 +0100

    chore: rework contexts setup (#139)

--- routes/contexts/contexts.go
+++ routes/contexts/contexts.go
@@ -2,10 +2,10 @@ package contexts
 
 import "context"
 
-func ExtractApp(ctx context.Context) *App {
-	appCtx, ok := ctx.Value(AppKey{}).(*App)
+func ExtractApp(ctx context.Context) App {
+	appCtx, ok := ctx.Value(AppKey{}).(App)
 	if !ok {
-		return &App{}
+		return App{}
 	}
 
 	return appCtx
