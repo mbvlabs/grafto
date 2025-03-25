9b56a694f444e3de38686121b711293f713ab539
mbv <github@mbvlabs.com>
2025-03-25 16:03:19 +0100

    feat: enable browser caching assets

--- routes/paths/assets.go
+++ routes/paths/assets.go
@@ -1,6 +1,6 @@
 package paths
 
-import "fmt"
+import "github.com/mbvlabs/grafto/static"
 
 const assetPrefix = "/assets"
 
@@ -8,19 +8,56 @@ var (
 	Robots = register(
 		Path{
 			Name: "assets.robots",
-			URL:  fmt.Sprintf("%s/robots.txt", assetPrefix),
+			URL:  "/robots.txt",
 		},
 	)
+
 	Sitemap = register(
 		Path{
 			Name: "assets.sitemap",
-			URL:  fmt.Sprintf("%s/sitemap.xml", assetPrefix),
+			URL:  "/sitemap.xml",
 		},
 	)
+
+	MainCss = register(
+		Path{
+			Name: "assets.main_css",
+			URL: func() string {
+				return assetPrefix + "/" + static.MainCssFile
+			}(),
+		},
+	)
+
+	HtmxJS = register(
+		Path{
+			Name: "assets.htmx",
+			URL:  assetPrefix + "/" + "htmx-2_0_4.min.js",
+		},
+	)
+
+	AlpineJS = register(
+		Path{
+			Name: "assets.alpine",
+			URL:  assetPrefix + "/" + "alpine-3_14_8.min.js",
+		},
+	)
+
 	Favicon = register(
 		Path{
 			Name: "assets.favicon",
-			URL:  fmt.Sprintf("%s/favicon.ico", assetPrefix),
+			URL:  "/favicon.ico",
+		},
+	)
+	Favicon16 = register(
+		Path{
+			Name: "assets.favicon_16_16",
+			URL:  assetPrefix + "/" + "favicon-16x16.png",
+		},
+	)
+	Favicon32 = register(
+		Path{
+			Name: "assets.favicon_32_32",
+			URL:  assetPrefix + "/" + "favicon-32x32.png",
 		},
 	)
 )
